1578f85743b907337d023a7fd697edfedb9314c3
proxysu <proxysetuptools@gmail.com>
2020-09-08 23:44:29 +0800

    fix dependencies bug3

--- ssr/ssr.sh
+++ ssr/ssr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-#PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-#export PATH
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 #=================================================================#
 #   System Required:  CentOS 7, 8, Debian 8,9,10, Ubuntu 16.04+   #
 #   Description: One click Install ShadowsocksR Server            #
@@ -533,7 +533,7 @@ uninstall_shadowsocksr(){
 # Install ShadowsocksR
 install_shadowsocksr(){
     # disable_selinux
-    #pre_install
+    pre_install
     ln -s /usr/bin/python3 /usr/bin/python
     ln -s /usr/bin/python /usr/bin/ssr
     download_files
